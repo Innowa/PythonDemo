cd6f0e0a36ffd041be8c18d7e431d10c052f87e4
Innowa <xieshuai421@gmail.com>
2018-04-23 16:28:00 +0800

    commit orm changed

--- src/LICENSE/orm.py
+++ src/LICENSE/orm.py
@@ -16,8 +16,8 @@ async def create_pool(loop, **kw):
     __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', '3306'),
-        user=kw['username'],
-        password=kw['password'],
+        user=kw['root'],
+        password=kw['123456'],
         db=kw['db'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
@@ -66,7 +66,7 @@ def create_args_string(num):
     return ','.join(l)
 
 
-class Filed(object):
+class Field(object):
 
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
@@ -78,37 +78,37 @@ class Filed(object):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
 
-class StringField(Filed):
+class StringField(Field):
 
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
 
-class BooleanField(Filed):
+class BooleanField(Field):
 
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
 
-class IntegerField(Filed):
+class IntegerField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
 
-class FloatField(Filed):
+class FloatField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
 
-class TextField(Filed):
+class TextField(Field):
 
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
 
-class ModeMetaclass(type):
+class ModelMetaclass(type):
 
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
@@ -119,10 +119,11 @@ class ModeMetaclass(type):
         fields = []
         primaryKey = None
         for k, v in attrs.items():
-            if isinstance(v, Filed):
+            if isinstance(v, Field):
                 logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
+                    # 找到主键:
                     if primaryKey:
                         raise BaseException('Duplicate primary key for field: %s' % k)
                     primaryKey = k
@@ -133,10 +134,10 @@ class ModeMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey
-        attrs['__fields__'] = fields
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
         attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
             tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
@@ -146,16 +147,16 @@ class ModeMetaclass(type):
         return type.__new__(cls, name, bases, attrs)
 
 
-class BaseModel(dict, metaclass=ModeMetaclass):
+class Model(dict, metaclass=ModelMetaclass):
 
     def __init__(self, **kw):
-        super(BaseModel, self).__init__(**kw)
+        super(Model, self).__init__(**kw)
 
-    def __getattr__(self, item):
+    def __getattr__(self, key):
         try:
-            return self[item]
+            return self[key]
         except KeyError:
-            raise AttributeError(r"'BaseModel' object has no attribute '%s'" % item)
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
     def __setattr__(self, key, value):
         self[key] = value
@@ -166,15 +167,16 @@ class BaseModel(dict, metaclass=ModeMetaclass):
     def getValueOrDefault(self, key):
         value = getattr(self, key, None)
         if value is None:
-            field = self.__mapping__[key]
+            field = self.__mappings__[key]
             if field.default is not None:
-                value = field.default() if callable(field.default) else field.defualt
+                value = field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
         return value
 
     @classmethod
     async def findAll(cls, where=None, args=None, **kw):
+        ' find objects by where clause. '
         sql = [cls.__select__]
         if where:
             sql.append('where')
@@ -186,11 +188,11 @@ class BaseModel(dict, metaclass=ModeMetaclass):
             sql.append('order by')
             sql.append(orderBy)
         limit = kw.get('limit', None)
-        if limit:
+        if limit is not None:
             sql.append('limit')
             if isinstance(limit, int):
                 sql.append('?')
-                sql.append(limit)
+                args.append(limit)
             elif isinstance(limit, tuple) and len(limit) == 2:
                 sql.append('?, ?')
                 args.extend(limit)
@@ -201,7 +203,8 @@ class BaseModel(dict, metaclass=ModeMetaclass):
 
     @classmethod
     async def findNumber(cls, selectField, where=None, args=None):
-        sql = ['select %s _num_from `%s`' % (selectField, cls.__table__)]
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
@@ -209,6 +212,14 @@ class BaseModel(dict, metaclass=ModeMetaclass):
         if len(rs) == 0:
             return None
         return rs[0]['_num_']
+
+    @classmethod
+    async def find(cls, pk):
+        ' find object by primary key. '
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
 
     async def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
